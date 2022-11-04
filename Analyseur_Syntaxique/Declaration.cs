f9380336456f5d0085f2b282f2aa6eb392f96bd3
Jérémy Veillette <1830942@cshawi.ca>
2022-11-04 09:08:14 -0400

    Declaration

---
+++ Analyseur_Syntaxique/Declaration.cs
@@ -0,0 +1,34 @@
+﻿namespace Analyseur_Syntaxique
+{
+    class Declaration
+    {
+        private Variable variable;
+        private string declare;
+        private Type type;
+
+        public Declaration(string decla, string var, string typ)
+        {
+            Setup(decla, var, typ);
+        }
+
+        private void Setup(string decla, string var, string typ)
+        {
+            declare = decla;
+            variable = new Variable(var);
+            switch (typ)
+            {
+                case "entier":
+                    type = Type.entier;
+                    break;
+                case "reel":
+                    type = Type.reel;
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return declare + " " + variable.ToString() + " : " + type.ToString() + ";";
+        }
+    }
+}
